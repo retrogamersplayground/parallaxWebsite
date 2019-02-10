037c4d023b05d373a6bbd77895ef5166975ebc8d
retrogamersplayground <neilfloyd99@gmail.com>
2019-02-09 23:33:20 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Parallax Website Demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="pimg1">
+        <div class="ptext">
+            <span class="border">
+                Design
+            </span>
+        </div>
+    </div>
+
+    <section class="section section-dark">
+        <h2>Who We Are</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        </p>
+    </section>
+
+    <div class="pimg2">
+        <div class="ptext">
+            <span class="border">
+                Build
+            </span>
+        </div>
+    </div>
+
+    <section class="section section-dark">
+        <h2>What We Do</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </p>
+    </section>
+
+    <div class="pimg3">
+        <div class="ptext">
+            <span class="border">
+                Connect
+            </span>
+        </div>
+    </div>
+
+    <section class="section section-dark">
+        <h2>Connect With Us</h2>
+        
+        <div class="contactContainer" id="contact">
+        
+            <div class="contactFormDiv">
+        
+                
+                <form method="post" action="parser.php">
+
+                    <input type="text" name="name" placeholder="NAME" id="name">
+                    <input type="email" name="email" placeholder="EMAIL" id="email">
+                    <textarea type="text" name="message" placeholder="MESSAGE" id="message"></textarea>
+
+                    <input type="submit" name="submit" value="CONNECT" id="submit">
+
+                </form>
+
+            </div> <!--contactForm-->
+
+        </div><!--contactContainer-->
+
+    </section>
+
+    <div class="pimg1">
+        <div class="ptext">
+            <span class="border">
+                Dominate
+            </span>
+        </div>
+    </div>
+    
+</body>
+</html>
